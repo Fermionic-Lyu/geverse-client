de4221cba41a0524edb1f08e19503978a5fdc521
xuguangxing <xugx29@163.com>
2024-11-28 13:01:18 +0800

    feat: create build script

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,8 +6,10 @@ import icon from '../../resources/icon.png?asset'
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 1600,
+    height: 900,
+    minWidth: 1600,
+    minHeight: 900,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
